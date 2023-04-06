b15d424b597b1684cc0697829ab60675e97abc3f
EEXstnce <enlightenedexistence@proton.me>
2023-04-06 10:57:44 -0500

    fixed requirements (hopefully)

--- code/test.py
+++ code/test.py
@@ -1,16 +1,58 @@
 
 
-import hashlib
+#import necessary modules
+import math
 
-# function to encode a string with SHA256
-def sha256_encoder(string):
-    return hashlib.sha256(string.encode()).hexdigest()
+#define functions
+def add(x, y):
+   return x + y
 
-# User input
-input_string = input('Please enter a string to be encoded: ')
+def subtract(x, y):
+   return x - y
 
-# Encoding
-encoded_string = sha256_encoder(input_string)
+def multiply(x, y):
+   return x * y
 
-# Output
-print('The encoded string is: ', encoded_string)
+def divide(x, y):
+   return x / y
+
+def power(x, y):
+   return x ** y
+
+def root(x, y):
+   return x ** (1/y)
+
+#take input from the user
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+print("5.Power")
+print("6.Root")
+
+choice = input("Enter choice(1/2/3/4/5/6):")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if choice == '1':
+   print(num1,"+",num2,"=", add(num1,num2))
+
+elif choice == '2':
+   print(num1,"-",num2,"=", subtract(num1,num2))
+
+elif choice == '3':
+   print(num1,"*",num2,"=", multiply(num1,num2))
+
+elif choice == '4':
+   print(num1,"/",num2,"=", divide(num1,num2))
+
+elif choice == '5':
+   print(num1,"**",num2,"=", power(num1,num2))
+
+elif choice == '6':
+   print(num1,"**(1/",num2,")=", root(num1,num2))
+
+else:
+   print("Invalid input")
